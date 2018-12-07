149023d5b265b8c3a9accc554adc345162a3101c
alon <alon@alon-laptop>
2018-12-07 13:47:11 +0200

    Add get BusinessID Feature with BusinessIdTO class

--- src/main/java/uncrowd/layout/WebUI.java
+++ src/main/java/uncrowd/layout/WebUI.java
@@ -54,13 +54,9 @@ public class WebUI {
 			method=RequestMethod.GET,
 			path="/BusinessID",
 			produces=MediaType.APPLICATION_JSON_VALUE)
-	public MessageTO getBusinessId (HttpServletRequest request) throws MessageNotFoundException {
+	public BusinessIdTO getBusinessId (HttpServletRequest request) throws MessageNotFoundException {
 		
-		System.out.println("\n\n\n************** Remote Address: " + request.getRemoteAddr());
-		
-		MessageEntity rv = this.messages.getMessage("BusinessID");
-		
-		return new MessageTO(rv);
+		return new BusinessIdTO();
 	}
 	
 	// Local host 
